8d4d236eddcf1fae4a7ae02e82165b9002bb391e
Ayushman5472 <61929617+Ayushman5472@users.noreply.github.com>
2021-01-02 12:58:59 +0530

    Add files via upload

---
+++ screens/RecieverDetailsScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import firebase from 'firebase'
+import{Text, View, TouchableOpacity} from 'react-native'
+
+export default class RecieverDetailsScreen extends React.Component{
+    render(){
+        return(
+            <Text>Reciever Details Here</Text>
+        )
+    }
+}
